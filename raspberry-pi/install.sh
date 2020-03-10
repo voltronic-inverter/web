135a0942cf4c2b65eb20aaba9355027db1a36ef8
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2020-03-10 16:53:03 +0200

    Update install script

--- raspberry-pi/install.sh
+++ raspberry-pi/install.sh
@@ -12,10 +12,20 @@ apt-get install -y gcc git autoconf automake libtool pkg-config libudev-dev libu
 
 # Checkout Fast CGI library
 mkdir /opt/voltronic-fcgi-interface
-mkdir /opt/voltronic-web
 chmod 775 /opt/voltronic-fcgi-interface
-chmod 775 /opt/voltronic-web
 git clone https://github.com/voltronic-inverter/fcgi-interface.git /opt/voltronic-fcgi-interface
+
+# Download supporting scripts & configurations
+mkdir /opt/voltronic-web
+chmod 775 /opt/voltronic-web
+wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/voltronic-fcgi-serial-control -O /opt/voltronic-web/voltronic-fcgi-serial-control
+wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/voltronic-fcgi-usb-control -O /opt/voltronic-web/voltronic-fcgi-usb-control
+wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/nginx.conf -O /etc/nginx/nginx.conf
+wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/voltronic-fcgi-control -O /etc/init.d/voltronic-fcgi-control
+
+chmod 775 /opt/voltronic-web/voltronic-fcgi-serial-control
+chmod 775 /opt/voltronic-web/voltronic-fcgi-usb-control
+
 cd /opt/voltronic-fcgi-interface/lib/
 ./pull_libfcgi2.sh
 ./pull_libhidapi.sh
@@ -47,3 +57,8 @@ cd /opt/voltronic-fcgi-interface/
 make serial
 make hidraw
 make libusb
+
+# FCGI startup scripts
+update-rc.d /etc/init.d/voltronic-fcgi-control defaults
+
+echo "Reboot your Raspberry Pi using 'sudo reboot now'"
