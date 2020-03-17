bd7dac0b4c1e5174ce5e5e000dd1938763319658
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2020-03-17 17:05:50 +0200

    Move udev rule & update HTML page

--- raspberry-pi/install.sh
+++ raspberry-pi/install.sh
@@ -15,17 +15,17 @@ git clone https://github.com/voltronic-inverter/fcgi-interface.git /opt/voltroni
 # Download supporting scripts & configurations
 mkdir /opt/voltronic-web
 chmod 775 /opt/voltronic-web
+wget https://raw.githubusercontent.com/voltronic-inverter/web/master/35-voltronic-udev.rules -O /etc/udev/rules.d/35-voltronic-udev.rules
 wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/voltronic-fcgi-serial-control -O /opt/voltronic-web/voltronic-fcgi-serial-control
 wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/voltronic-fcgi-usb-control -O /opt/voltronic-web/voltronic-fcgi-usb-control
 wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/voltronic-fcgi-control -O /etc/init.d/voltronic-fcgi-control
-wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/85-voltronic.rules -O /etc/udev/rules.d/85-voltronic.rules
 wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/nginx.conf -O /etc/nginx/nginx.conf
 wget https://raw.githubusercontent.com/voltronic-inverter/web/master/raspberry-pi/command.html -O /usr/share/nginx/html/command.html
 
+chmod 664 /etc/udev/rules.d/35-voltronic-udev.rules
 chmod 775 /opt/voltronic-web/voltronic-fcgi-serial-control
 chmod 775 /opt/voltronic-web/voltronic-fcgi-usb-control
 chmod 775 /etc/init.d/voltronic-fcgi-control
-chmod 664 /etc/udev/rules.d/85-voltronic.rules
 chmod 664 /etc/nginx/nginx.conf
 chmod 664 /usr/share/nginx/html/command.html
 
