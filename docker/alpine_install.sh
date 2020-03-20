65f166ba9fd91b7e3eae4cb7e84f6e2c98d7c0ed
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2020-03-20 17:07:43 +0200

    Add daemon scripts

--- docker/alpine_install.sh
+++ docker/alpine_install.sh
@@ -31,13 +31,24 @@ make serial
 make hidraw
 make libusb
 
+mv /opt/voltronic-fcgi-interface/voltronic_fcgi_serial /opt/voltronic_fcgi_serial
 mv /opt/voltronic-fcgi-interface/voltronic_fcgi_hidraw /opt/voltronic_fcgi_hidraw
 mv /opt/voltronic-fcgi-interface/voltronic_fcgi_libusb /opt/voltronic_fcgi_libusb
-mv /opt/voltronic-fcgi-interface/voltronic_fcgi_serial /opt/voltronic_fcgi_serial
 
+echo -e '#!/usr/bin/env sh\n\nwhile true; do\nspawn-fcgi -p 9001 -n /opt/voltronic_fcgi_serial\ndone' > /opt/voltronic_fcgi_serial_daemon
+echo -e '#!/usr/bin/env sh\n\nwhile true; do\nspawn-fcgi -p 9002 -n /opt/voltronic_fcgi_hidraw\ndone' > /opt/voltronic_fcgi_hidraw_daemon
+echo -e '#!/usr/bin/env sh\n\nwhile true; do\nspawn-fcgi -p 9003 -n /opt/voltronic_fcgi_libusb\ndone' > /opt/voltronic_fcgi_libusb_daemon
+echo -e '#!/usr/bin/env sh\n\n/opt/voltronic_fcgi_serial_daemon &\n/opt/voltronic_fcgi_hidraw_daemon &\n/opt/voltronic_fcgi_libusb_daemon &\n\nwhile true; do\n  sleep 1h\ndone
+' > /opt/voltronic_fcgi_daemon
+
+chmod 775 /opt/voltronic_fcgi_serial
 chmod 775 /opt/voltronic_fcgi_hidraw
 chmod 775 /opt/voltronic_fcgi_libusb
-chmod 775 /opt/voltronic_fcgi_serial
+
+chmod 775 /opt/voltronic_fcgi_serial_daemon
+chmod 775 /opt/voltronic_fcgi_hidraw_daemon
+chmod 775 /opt/voltronic_fcgi_libusb_daemon
+chmod 775 /opt/voltronic_fcgi_daemon
 
 rm -rf /opt/voltronic-fcgi-interface
 
