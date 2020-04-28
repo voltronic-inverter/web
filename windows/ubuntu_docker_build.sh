6681ee2e834473acd63c1c924965e1f7934cee1c
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2020-04-28 16:38:23 +0200

    Fix stupid off by 1 error

--- windows/ubuntu_docker_build.sh
+++ windows/ubuntu_docker_build.sh
@@ -16,16 +16,15 @@ echo "Starting build"
 apt-get install -y make gcc autoconf automake libtool pkg-config mingw-w64
 
 # Start the build loop
-LOOP_COUNT=1
-LOOP_STATE=0
-while [ $LOOP_COUNT -le 1 ]; do
-  LOOP_STATE=$(( $LOOP_STATE + 1 ))
+LOOP_COUNT=0
+while [ $LOOP_COUNT -le 2 ]; do
+  LOOP_COUNT=$(( $LOOP_COUNT + 1 ))
 
   rm -rf '/build' 1>/dev/null 2>/dev/null
 
-  if [ $LOOP_STATE -eq 1 ]; then
+  if [ $LOOP_COUNT -eq 1 ]; then
     echo "Building i686 binaries"
-  elif [ $LOOP_STATE -eq 2 ]; then
+  elif [ $LOOP_COUNT -eq 2 ]; then
     echo "Building x86-64 binaries"
   else
     echo "Unsupported build mode"
@@ -36,7 +35,7 @@ while [ $LOOP_COUNT -le 1 ]; do
   # Build libserialport
   cd '/build/fcgi-interface/lib/libvoltronic/lib/libserialport'
   ./autogen.sh
-  if [ $LOOP_STATE -eq 1 ]; then
+  if [ $LOOP_COUNT -eq 1 ]; then
     ./configure --host=i686-w64-mingw32 --target=xi686-w64-mingw32 --disable-dependency-tracking
   else
     ./configure --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-dependency-tracking
@@ -46,7 +45,7 @@ while [ $LOOP_COUNT -le 1 ]; do
   # Build HIDAPI
   cd '/build/fcgi-interface/lib/libvoltronic/lib/hidapi'
   ./bootstrap
-  if [ $LOOP_STATE -eq 1 ]; then
+  if [ $LOOP_COUNT -eq 1 ]; then
     ./configure --host=i686-w64-mingw32 --target=i686-w64-mingw32
   else
     ./configure --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32
@@ -56,7 +55,7 @@ while [ $LOOP_COUNT -le 1 ]; do
   # Build fcgi2
   cd '/build/fcgi-interface/lib/fcgi2'
   ./autogen.sh
-  if [ $LOOP_STATE -eq 1 ]; then
+  if [ $LOOP_COUNT -eq 1 ]; then
     ./configure --host=i686-w64-mingw32 --target=i686-w64-mingw32
   else
     ./configure --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32
@@ -66,27 +65,25 @@ while [ $LOOP_COUNT -le 1 ]; do
   # Build fcgi-interface
   cd '/build/fcgi-interface'
   rm -f Makefile
-  if [ $LOOP_STATE -eq 1 ]; then
+  if [ $LOOP_COUNT -eq 1 ]; then
     curl -L -o '/build/fcgi-interface/Makefile' 'https://raw.githubusercontent.com/voltronic-inverter/web/master/windows/Makefile_x86'
   else
     curl -L -o '/build/fcgi-interface/Makefile' 'https://raw.githubusercontent.com/voltronic-inverter/web/master/windows/Makefile_x86_64'
   fi
 
   make clean && make libserialport
-  if [ $LOOP_STATE -eq 1 ]; then
+  if [ $LOOP_COUNT -eq 1 ]; then
     mv -f '/build/fcgi-interface/libserialport.exe' '/io/voltronic_fcgi_libserialport_i686.exe'
   else
     mv -f '/build/fcgi-interface/libserialport.exe' '/io/voltronic_fcgi_libserialport_x86-64.exe'
   fi
 
   make clean && make hidapi
-  if [ $LOOP_STATE -eq 1 ]; then
+  if [ $LOOP_COUNT -eq 1 ]; then
     mv -f '/build/fcgi-interface/hidapi.exe' '/io/voltronic_fcgi_hidapi_hidraw_i686.exe'
   else
     mv -f '/build/fcgi-interface/hidapi.exe' '/io/voltronic_fcgi_hidapi_hidraw_x86-64.exe'
   fi
-
-  LOOP_COUNT=$(( $LOOP_COUNT + 1 ))
 done
 
 echo "Build complete"
