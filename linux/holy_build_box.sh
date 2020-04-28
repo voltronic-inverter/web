c19fadada2a2ffcd91d61e7b8cc27c73d4d42bdb
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2020-04-28 16:22:36 +0200

    Switch to more reliable variable checking

--- linux/holy_build_box.sh
+++ linux/holy_build_box.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
 # Fetch all the repos
+mkdir '/src'
 curl -L -o '/src/repo_fetcher.sh' 'https://raw.githubusercontent.com/voltronic-inverter/web/master/shared/repo_fetcher.sh'
 chmod 775 '/src/repo_fetcher.sh'
 /src/repo_fetcher.sh
 
 # Determine target platform
+BUILD_STATE=0
 uname -m | grep 'i686' > /dev/null 2>/dev/null
 if [ $? -eq 0 ]; then
-  echo 'Building i686 Linux binaries'
-  declare -i IS_X86_64=0
+  BUILD_STATE=$(( $BUILD_STATE + 1 ))
 else
-  declare -i IS_X86_64=1
+  BUILD_STATE=$(( $BUILD_STATE + 2 ))
+fi
+
+if [ $BUILD_STATE -eq 1 ]; then
+  echo 'Building i686 Linux binaries'
+elif [ $BUILD_STATE -eq 2 ]; then
   echo 'Building x86-64 Linux binaries'
+else
+  echo 'Unknown build state'
+  exit 1
 fi
 
 # Install udev; We will not be linking to it statically
 yum install -y libudev libudev-devel
-if [ $IS_X86_64 -eq 0 ]; then
+if [ $BUILD_STATE -eq 0 ]; then
   ln -s /usr/lib/pkgconfig/libudev.pc /hbb_exe/lib/pkgconfig/
 else
   ln -s /usr/lib64/pkgconfig/libudev.pc /hbb_exe/lib/pkgconfig/
@@ -44,7 +53,7 @@ cd /build/fcgi-interface/lib/libvoltronic/lib/libusb/
 ./configure
 make
 make install
-if [ $IS_X86_64 -eq 0 ]; then
+if [ $BUILD_STATE -eq 0 ]; then
   ln -s /usr/local/lib/pkgconfig/libusb-1.0.pc /hbb_exe/lib/pkgconfig/
 else
   ln -s /usr/local/lib/pkgconfig/libusb-1.0.pc /hbb_exe/lib/pkgconfig/
@@ -68,14 +77,14 @@ rm -f Makefile
 curl -L -o '/build/fcgi-interface/Makefile' 'https://raw.githubusercontent.com/voltronic-inverter/web/master/linux/Makefile'
 
 make clean && make libserialport
-if [ $IS_X86_64 -eq 0 ]; then
+if [ $BUILD_STATE -eq 0 ]; then
   mv -f '/build/fcgi-interface/voltronic_fcgi_libserialport' '/io/voltronic_fcgi_libserialport_i686'
 else
   mv -f '/build/fcgi-interface/voltronic_fcgi_libserialport' '/io/voltronic_fcgi_libserialport_x86-64'
 fi
 
 make clean && make hidapi-hidraw
-if [ $IS_X86_64 -eq 0 ]; then
+if [ $BUILD_STATE -eq 0 ]; then
   mv -f '/build/fcgi-interface/voltronic_fcgi_hidapi_hidraw' '/io/voltronic_fcgi_hidapi_hidraw_i686'
 else
   mv -f '/build/fcgi-interface/voltronic_fcgi_hidapi_hidraw' '/io/voltronic_fcgi_hidapi_hidraw_x86-64'
